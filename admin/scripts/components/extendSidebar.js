810d1d5d242261e5097fee3816cde1ae4062bbc3
KhoaDang0702 <23130041@st.hcmuaf.edu.vn>
2025-11-10 17:27:24 +0700

    Thêm trang chi tiết sản phẩm và comment các file

--- admin/scripts/components/extendSidebar.js
+++ admin/scripts/components/extendSidebar.js
@@ -3,6 +3,7 @@ const menuToggle = document.getElementById('menu__action');
 const sidebar = document.querySelector('.sidebar');
 const maincomponent = document.querySelector('.main');
 
+// Add/remove class collapsed to sidebar and container to css when shrink or expand the sidebar
 if (menuToggle && sidebar && maincomponent) {
     menuToggle.addEventListener('click', () => {
         sidebar.classList.toggle('collapsed');
